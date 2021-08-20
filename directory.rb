75f8fd3cbd32e388f76a14a4d00c84fa6afb607c
Sophie Bonneval <maldorana@gmail.com>
2021-08-20 09:59:41 +0100

    Add interface menu

--- directory.rb
+++ directory.rb
@@ -2,33 +2,59 @@
 
 def input_students
   students = []
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-  puts "Please enter a student name"
-  puts "Hit enter twice to finish"
-  name = gets.chomp
-
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "We now have #{students.count} student(s). Next student name? (Hit enter twice to finish)"
+  while true do
+    puts "Please enter a student name (leave blank to finish)"
     name = gets.chomp
+    return students if name.empty?
+    puts "Please enter this student's cohort"
+    cohort = gets.chomp
+    if cohort.empty?
+      cohort = :November
+    elsif !months.include?(cohort.capitalize)
+      puts "That's not a month. Reverting to default value."
+      cohort = :November
+    else
+      cohort.capitalize.to_sym
+    end
+ 
+     students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "We now have #{students.count} student."
+    elsif students.count > 1
+      puts "We now have #{students.count} students."
+    end
   end
 
   students
 end
 
-# Define methods
+# Output Students
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "----------------------------".center(50, "-")
+  puts " The students of Villains Academy ".center(50, "-")
+  puts "----------------------------".center(50, "-")
 end
 
 def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  cohorts = []
+
+  students.each { |student| cohorts << student[:cohort].to_s.capitalize if !cohorts.include?(student[:cohort].to_s.capitalize) }
+  
+  cohorts.each do |cohort|
+    puts cohort.upcase.center(50, "-")
+    students.each do |student|
+      puts student[:name].center(50) if student[:cohort].to_s.capitalize == cohort
+    end
+  end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "-".center(50, "-")
+  puts "Overall, we have #{students.count} great students".center(50)
+  puts "-".center(50, "-")
 end
 
 # Call methods
